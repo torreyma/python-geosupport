2cfa4bd5e9e42773718a795b920923dc2db8af03
Mark Torrey <mtorrey@health.nyc.gov>
2023-05-02 15:18:14 -0400

    cleared out libary options that won't work

--- MT-local-lib-config-commands.sh
+++ MT-local-lib-config-commands.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 ## Geosupport version 23a:
-# export LD_LIBRARY_PATH=$HOME/DOHMH-local/geosupport_version-23a_23.1/lib/
-# export GEOFILES=$HOME/DOHMH-local/geosupport_version-23a_23.1/fls/
-## test from home dir instead of mounted drive:
 # export LD_LIBRARY_PATH=$HOME/geosupport_version-23a_23.1/lib/
 # export GEOFILES=$HOME/geosupport_version-23a_23.1/fls/
 
 ## Geosupport version 21c:
-# export LD_LIBRARY_PATH=$HOME/DOHMH-local/geosupport_version-21c_21.3/lib/
-# export GEOFILES=$HOME/DOHMH-local/geosupport_version-21c_21.3/fls/
+# export LD_LIBRARY_PATH=$HOME/geosupport_version-21c_21.3/lib/
+# export GEOFILES=$HOME/geosupport_version-21c_21.3/fls/
 
 
 
